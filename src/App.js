0077100d1ca2b7f27dcfb89da2a0a0259c8c29c2
Denisse Orellana <denissep.orellana@gmail.com>
2022-02-19 18:36:38 -0300

    add: styles to index.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   const title = 'Welcome to the new blog';
 
   return (
     <div className="App">
+      <Navbar /> 
       <div className="content">
-        <Navbar /> 
-        <h1>{ title }</h1> 
+        <Home />
       </div>
     </div>
   );
