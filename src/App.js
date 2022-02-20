a79e5ae1b90ef14c23768ffc22bc4d9b20c53a6f
Denisse Orellana <denissep.orellana@gmail.com>
2022-02-20 12:48:25 -0300

    add: routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   const title = 'Welcome to the new blog';
 
   return (
-    <div className="App">
-      <Navbar /> 
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar /> 
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
