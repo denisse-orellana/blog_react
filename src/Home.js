4ae7019dcca97a4b2615c6afa0d59e5e4111438f
Denisse Orellana <denissep.orellana@gmail.com>
2022-02-20 11:33:30 -0300

    add: error message

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,30 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
 
     setTimeout(() => {
 
       fetch('http://localhost:8000/blogs')
-      .then(resp => {
-        return resp.json();
-      })
-      .then(data => {
-        // console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-      });
+        .then(res => {
+          console.log(res)
+          if (!res.ok) {
+            throw Error('Could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then(data => {
+          // console.log(data);
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          setIsPending(false);
+          setError(err.message);
+        })
 
     }, 1000 );
 
@@ -26,6 +36,7 @@ const Home = () => {
 
   return ( 
     <div className="home">
+      { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
       { blogs && <BlogList blogs={ blogs } title="All blogs" /> }
     </div>
